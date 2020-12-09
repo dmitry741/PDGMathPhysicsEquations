595d59f2112f1cc21c959b1d8422a945c5701b13
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-12-09 17:37:30 +0300

    Begin develope GUI for HeatConductivity

---
+++ PDGMathPhysicsEquations/WinHeatConductivity/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WinHeatConductivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(906, 257);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 32);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(230, 257);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(90, 32);
+            this.btnPlay.TabIndex = 8;
+            this.btnPlay.Text = "Старт";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 257);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(212, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(326, 257);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(90, 32);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "Шаг++";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 239);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 293);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Распространение тепла в стержне";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PDGMathPhysicsEquations/WinHeatConductivity/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathPhysicsEq;
+using PDGMathPhysicsEquations;
+
+namespace WinHeatConductivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region === members ===
+
+        Bitmap _bitmap;
+        Timer _timer;
+        double _curTime;
+
+        const double _cL = 2.0;
+        const double _cTimeStep = 0.025;
+
+        #endregion
+
+        #region === private methods ===
+
+        void Next()
+        {
+            //_curTime += _cTimeStep;
+            //FunctionProperties fp = (FunctionProperties)comboBox1.SelectedItem;
+
+            // TODO
+        }
+
+
+        void RenderKernel(Graphics g)
+        {
+            // TODO:
+        }
+
+        void Render()
+        {
+            // проверка картинки на валидность.
+            if (_bitmap == null)
+                return;
+
+            // создаем новый графический контекст.
+            Graphics g = Graphics.FromImage(_bitmap);
+
+            // очищаем контекст.
+            g.Clear(Color.White);
+
+            // отрисовка стержня
+            RenderKernel(g);
+
+            // отрисовка.
+            pictureBox1.Image = _bitmap;
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            _curTime = 0;
+
+            // создаем таймер
+            _timer = new Timer
+            {
+                Interval = 500,
+                Enabled = false
+            };
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Next();
+            Render();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_timer.Enabled)
+                _timer.Stop();
+        }
+    }
+}
